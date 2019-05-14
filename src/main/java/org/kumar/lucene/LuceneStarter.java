73e88fcad00e0eb04b494d73bd8817f6cf1f5d91
ashishchauhan99 <ashish.chauhan99@gmail.com>
2019-05-14 10:57:51 +0200

    project setup

---
+++ src/main/java/org/kumar/lucene/LuceneStarter.java
@@ -0,0 +1,11 @@
+package org.kumar.lucene;
+
+
+public class LuceneStarter {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/test/java/org/kumar/lucene/LuceneStarterTest.java
@@ -0,0 +1,5 @@
+package org.kumar.lucene;
+
+public class LuceneStarterTest {
+
+}
